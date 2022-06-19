14e82fd10c271b9c1cf83361a3ecb253376b7249
GamerUthsav <105775576+GamerUthsav@users.noreply.github.com>
2022-06-19 10:04:50 +0530

    Add files via upload

---
+++ p5play_template/sketch.js
@@ -0,0 +1,20 @@
+var uthsav;
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(400,400);
+ uthsav = createSprite(200,200,100,100);
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
